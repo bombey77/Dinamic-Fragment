b6de5b690cd77976ace635831fa15929043cf618
Motovilov Roman <bombey131@gmail.com>
2017-08-14 22:03:23 +0200

    create buttons remove, replace and switch

--- app/src/main/java/bombey77/motovilov/com/dinamicfragment/MainActivity.java
+++ app/src/main/java/bombey77/motovilov/com/dinamicfragment/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private FragmentOne fragmentOne;
     private FragmentTwo fragmentTwo;
 
+    private Switch isBackStack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
         fragmentOne = new FragmentOne();
         fragmentTwo = new FragmentTwo();
 
+        isBackStack = (Switch) findViewById(R.id.switchIsBackStack);
+
         fragmentManager = getSupportFragmentManager();
     }
 
-    public void onFirst(final View view) {
+    public void onClick(final View view) {
 
         fragmentTransaction = fragmentManager.beginTransaction();
 
@@ -33,17 +38,38 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btnFirst:
                 if (fragmentManager.findFragmentByTag(FragmentOne.TAG) == null) {
                     fragmentTransaction.add(R.id.container, fragmentOne, FragmentOne.TAG);
-                    fragmentTransaction.commit();
                 }
                 break;
 
             case R.id.btnSecond:
                 if (fragmentManager.findFragmentByTag(FragmentTwo.TAG) == null) {
                     fragmentTransaction.add(R.id.container, fragmentTwo, FragmentTwo.TAG);
-                    fragmentTransaction.commit();
+                }
+                break;
+
+            case R.id.btnRemove:
+                if (fragmentManager.findFragmentByTag(FragmentOne.TAG) != null) {
+                    fragmentTransaction.remove(fragmentOne);
+                }
+                if (fragmentManager.findFragmentByTag(FragmentTwo.TAG) != null) {
+                    fragmentTransaction.remove(fragmentTwo);
+                }
+                break;
+
+            case R.id.btnReplace:
+                if (fragmentManager.findFragmentByTag(FragmentOne.TAG) != null) {
+                    fragmentTransaction.replace(R.id.container, fragmentTwo, FragmentTwo.TAG);
+                }
+                if (fragmentManager.findFragmentByTag(FragmentTwo.TAG) != null) {
+                    fragmentTransaction.replace(R.id.container, fragmentOne, FragmentOne.TAG);
                 }
                 break;
         }
 
+        if (isBackStack.isChecked()) {
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        fragmentTransaction.commit();
     }
 }
